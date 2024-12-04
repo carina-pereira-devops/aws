4f3af5bab4084992d48843fd755ce45dc3aa99f3
carina-pereira-devops <devops79@yahoo.com>
2024-12-04 18:48:00 -0300

    Retirando a configuração de Count

--- iac_eks/Main.tf
+++ iac_eks/Main.tf
@@ -141,7 +141,6 @@ resource "aws_route_table_association" "public-us-east-1b" {
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.otel.id
 
-  count = 2
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.natgateway.id
