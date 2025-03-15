721985d832272cdc4117257d88d722206d5bd379
carina-pereira-devops <devops79@yahoo.com>
2025-03-14 21:42:52 -0300

    Deleção

--- iac_eks/modules.tf
+++ iac_eks/modules.tf
@@ -36,3 +36,30 @@ module "nodes" {
 
   auto_scale_cpu     = var.auto_scale_cpu
 }
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "~> 20.0"
+
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::535002861869:role/k8s-otel-eks-cluster-role"
+      username = "k8s-otel-eks-cluster-role"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::535002861869:user/devops"
+      username = "devops"
+      groups   = ["system:masters"]
+    }
+  ]
+
+  aws_auth_accounts = [
+    "535002861869",
+  ]
+}
